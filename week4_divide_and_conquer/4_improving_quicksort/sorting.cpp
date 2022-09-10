bdcb4773fb68882a4570cb2052cdb2c7fc43f307
Abdelrahman_Abdelmougeth <abdelrahmana966@gmail.com>
2022-09-10 23:52:01 +0200

    04-Improving Quicksort

--- week4_divide_and_conquer/4_improving_quicksort/sorting.cpp
+++ week4_divide_and_conquer/4_improving_quicksort/sorting.cpp
@@ -5,31 +5,78 @@
 using std::vector;
 using std::swap;
 
-int partition2(vector<int> &a, int l, int r) {
-  int x = a[l];
-  int j = l;
-  for (int i = l + 1; i <= r; i++) {
-    if (a[i] <= x) {
-      j++;
-      swap(a[i], a[j]);
-    }
+// This function takes last element as
+// pivot, places the pivot element at
+// its correct position in sorted
+// array, and places all smaller
+// (smaller than pivot) to left of pivot
+// and all greater elements to right
+int partition(vector<int> &arr, int low, int high)
+{
+  int pivot = arr[low];
+  int i = low - 1, j = high + 1;
+
+  while (true)
+  {
+
+    // Find leftmost element greater than
+    // or equal to pivot
+    do
+    {
+      i++;
+    } while (arr[i] < pivot);
+
+    // Find rightmost element smaller than
+    // or equal to pivot
+    do
+    {
+      j--;
+    } while (arr[j] > pivot);
+
+    // If two pointers met
+    if (i >= j)
+      return j;
+
+    swap(arr[i], arr[j]);
   }
-  swap(a[l], a[j]);
-  return j;
 }
 
-void randomized_quick_sort(vector<int> &a, int l, int r) {
-  if (l >= r) {
-    return;
-  }
+// Generates Random Pivot, swaps pivot with
+// end element and calls the partition function
+// In Hoare partition the low element is selected
+// as first pivot
+int partition_r(vector<int> &arr, int low, int high)
+{
+  // Generate a random number in between
+  // low .. high
+  srand(time(NULL));
+  int random = low + rand() % (high - low);
 
-  int k = l + rand() % (r - l + 1);
-  swap(a[l], a[k]);
-  int m = partition2(a, l, r);
+  // Swap A[random] with A[high]
+  swap(arr[random], arr[low]);
 
-  randomized_quick_sort(a, l, m - 1);
-  randomized_quick_sort(a, m + 1, r);
+  return partition(arr, low, high);
 }
+
+// The main function that implements QuickSort
+// arr[] --> Array to be sorted,
+// low  --> Starting index,
+// high  --> Ending index
+void quickSort(vector<int> &arr, int low, int high)
+{
+  while (low < high)
+  {
+    // pi is partitioning index,
+    // arr[p] is now at right place
+    int pi = partition_r(arr, low, high);
+
+    // Separately sort elements before
+    // partition and after partition
+    quickSort(arr, low, pi);
+    low = pi + 1;
+  }
+}
+
 
 int main() {
   int n;
@@ -38,7 +85,7 @@ int main() {
   for (size_t i = 0; i < a.size(); ++i) {
     std::cin >> a[i];
   }
-  randomized_quick_sort(a, 0, a.size() - 1);
+  quickSort(a, 0, a.size() - 1);
   for (size_t i = 0; i < a.size(); ++i) {
     std::cout << a[i] << ' ';
   }
