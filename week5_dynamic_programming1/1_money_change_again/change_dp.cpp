1b55c4836f495d0896dd9eb4b020e4b3a779e660
Abdelrahman_Abdelmougeth <abdelrahmana966@gmail.com>
2022-09-29 20:36:53 +0200

    01-Money Change

--- week5_dynamic_programming1/1_money_change_again/change_dp.cpp
+++ week5_dynamic_programming1/1_money_change_again/change_dp.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 
-int get_change(int m) {
-  //write your code here
-  return m / 4;
+int get_change(int m)
+{
+  // write your code here
+  int coins[3] = {1, 3, 4};
+  int DP[m + 1];
+  for (int i = 0; i < m + 1; i++)
+    DP[i] = INT16_MAX;
+  DP[0] = 0;
+
+  for (int i = 1; i <= m; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      if (coins[j] <= i)
+      {
+        int sub_change = DP[i - coins[j]] + 1;
+        if (sub_change != INT16_MAX)
+          DP[i] = std::min(DP[i], sub_change);
+      }
+    }
+  }
+  return DP[m];
 }
 
-int main() {
+int main()
+{
   int m;
   std::cin >> m;
   std::cout << get_change(m) << '\n';
